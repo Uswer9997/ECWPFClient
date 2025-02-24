84169d6afcb25eff31eaa7d172962f9a35c29c99
adiutor62 <adiutor62@gmail.com>
2025-02-24 20:46:30 +0300

    Создана VM главного окна

---
+++ ECWPFClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ECWPFClient.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECWPFClient.ViewModels
+{
+  internal class MainWindowViewModel : BaseViewModel
+  {
+    private string _Title = "EC Client";
+
+    public string Title
+    {
+      get => _Title;
+      set => Set(ref _Title, value);
+    }
+  }
+}
